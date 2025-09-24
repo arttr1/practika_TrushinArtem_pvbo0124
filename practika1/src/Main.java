35ab59acc257b7c6993b587cc9a08bb0c2b809b1
arttr1 <arttr170bu@gmail.com>
2025-09-24 10:20:20 +0300

    practika1 done

--- practika1/src/Main.java
+++ practika1/src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Formatter;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+//метод из задания 7
+    static int fact(int n){
+        int res = 1;
+        if (n == 0 || n == 1){
+            return res;
         }
+        for (int i = 2; i <= n; i++){
+            res *= i;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Formatter fmt = new Formatter();
+
+
+        //задание 3
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        float sum = 0, average;
+
+        for (int i = 0; i < n; i++){
+            arr[i] = input.nextInt();
+            sum += arr[i];
+        }
+        average = sum / n;
+        System.out.println("sum = " + sum + "\n" + "avarage = " + average);
+
+        //задание 6
+        for (int i = 1; i < 11; i++){
+            System.out.printf("%.2f ", 1.0/i);
+        }
+
+        //задание 7
+        System.out.println(fact(5));
+
+
+        input.close();
     }
 }
