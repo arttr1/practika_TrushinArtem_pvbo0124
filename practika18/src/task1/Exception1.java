171c4f967360281298bf3dba9c124a75ad8f6421
artem trushin <arttr@artems-MacBook-Air.local>
2025-11-28 22:19:43 +0300

    task1 done

---
+++ practika18/src/task1/Exception1.java
@@ -0,0 +1,5 @@
+public class Exception1 {
+    public void exceptionDemo() {
+        System.out.println( 2 / 0 );
+    }
+} 
---
+++ practika18/src/task1/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Exception1 excep = new Exception1();
+        excep.exceptionDemo(); 
+    }
+}
